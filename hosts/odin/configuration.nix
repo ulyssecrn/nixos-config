ea7362d8403782f073d2de9753e56dbd346e61b9
Ulysse Corne <ulysse@corne.sh>
2025-04-27 19:23:41 +0200

    [odin] add greetd

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -84,6 +84,15 @@
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
   services.gvfs.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd Hyprland";
+        user = "greeter";
+      };
+    };
+  };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nixpkgs.config.allowUnfree = true;
