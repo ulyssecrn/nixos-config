939cc6e699b07480f01063ab04d90c3e86948b01
Ulysse Corne <ulysse@corne.sh>
2025-06-04 23:40:37 +0200

    added french locale

--- hosts/genghis/configuration.nix
+++ hosts/genghis/configuration.nix
@@ -36,6 +36,9 @@
   time.timeZone = "Europe/Paris";
   i18n.defaultLocale = "en_US.UTF-8";
 
+  i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8"];
+
+
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "fr_FR.UTF-8";
     LC_IDENTIFICATION = "fr_FR.UTF-8";
