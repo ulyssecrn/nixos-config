c8e79f5f65bb25544f6019a512e863c63e92b850
Ulysse Corne <ulysse@corne.sh>
2025-07-30 00:43:53 +0200

    [genghis] increase hyprlock timeout

--- hosts/genghis/hyprland.nix
+++ hosts/genghis/hyprland.nix
@@ -12,11 +12,11 @@
 
       listener = [
         {
-          timeout = 120;
+          timeout = 30;
           on-timeout = "hyprlock";
         }
         {
-          timeout = 300;
+          timeout = 600;
           on-timeout = "hyprctl dispatch dpms off";
           on-resume = "hyprctl dispatch dpms on";
         }
