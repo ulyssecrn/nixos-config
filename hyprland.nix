0b77469fbc0caaf4c77c9e23740415553865fd41
Ulysse Corne <ulysse@corne.sh>
2025-10-03 15:44:53 +0200

    [hyprland] enable xdg portal (needed for display recording)

--- hyprland.nix
+++ hyprland.nix
@@ -68,6 +68,19 @@ in
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      hyprland = {
+        default = [ "hyprland" "gtk" ];
+      };
+    };
+  };
+
   programs.hyprlock = {
     enable = true;
     settings = {
