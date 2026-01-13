b71d2efaf3ad1dff98325f778f3c388e66138057
Ulysse Corne <ulysse@corne.sh>
2026-01-13 18:51:44 -0500

    [odin] change time zone to NY

--- hosts/odin/configuration.nix
+++ hosts/odin/configuration.nix
@@ -51,7 +51,7 @@
   };
   */
 
-  time.timeZone = "Europe/Paris";
+  time.timeZone = "America/New_York";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.supportedLocales = ["en_US.UTF-8/UTF-8" "fr_FR.UTF-8/UTF-8"];
   
