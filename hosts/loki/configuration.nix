7c17188c7c3e2ad20ec97940e04c170c44c47e40
Ulysse Corne <ulysse@corne.sh>
2026-01-20 22:01:25 -0500

    [loki] initial config

---
+++ hosts/loki/configuration.nix
@@ -0,0 +1,189 @@
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../../dev.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot = {
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+
+    plymouth = {
+      enable = true;
+      theme = "nixos-bgrt";
+      themePackages = [pkgs.nixos-bgrt-plymouth];
+    };
+
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    initrd.systemd.enable = true; # enable to have a gui for encryption password input
+    kernelParams = [ 
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "rd.systemd.show_status=auto"
+      "rd.udev.log_level=3"
+      "udev.log_priority=3"
+    ];
+  };
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "loki"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "America/New_York";
+  i18n.defaultLocale = "en_US.UTF-8";
+  services.xserver.xkb.layout = "us";
+
+  hardware.graphics.enable = true;
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+
+  services.blueman.enable = true;
+
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = true;
+
+  users.users.ucorne = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ]; # Enable ‘sudo’ for the user.
+    packages = with pkgs; [];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+  ];
+
+  programs.firefox.enable = true;
+
+  nixpkgs.overlays = [ (
+    final: prev: {
+      # FreeCAD QT platform fix
+      freecad = prev.freecad.overrideAttrs (_old: {
+        postFixup = ''
+          wrapProgram $out/bin/FreeCAD --set QT_QPA_PLATFORM xcb
+        '';
+      });
+      # Dolphin fix for MIME apps support
+      kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
+          dolphin = kprev.dolphin.overrideAttrs (oldAttrs: {
+            nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
+            postInstall = (oldAttrs.postInstall or "") + ''
+              wrapProgram $out/bin/dolphin \
+                  --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
+                  --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
+            '';
+          });
+        });
+    }
+    ) 
+  ];
+
+  qt = {
+    enable = true;
+    platformTheme = "qt5ct";
+    style = "kvantum";
+  };
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
+
+  programs.zsh.enable = true;
+
+  security.polkit.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  services.gvfs.enable = true;
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start default'";
+        user = "greeter";
+      };
+    };
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
+
+  programs.steam = {
+    enable = true;
+    extest.enable = true; # controller mouse support on wayland
+    localNetworkGameTransfers.openFirewall = true;
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
+  };
+
+  services.printing.enable = true;
+
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = [
+      "db64858fed6d7cac"
+    ];
+  };
+
+  programs.nix-ld.enable = true; # unpatched dynamic libraries support
+
+  programs.virt-manager.enable = true;
+  users.groups.libvirtd.members = ["ucorne"];
+  users.groups.kvm.members = [ "ucorne" ];
+
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+    };
+  };
+  virtualisation.spiceUSBRedirection.enable = true;
+
+  hardware.ledger.enable = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "25.11"; # Did you read the comment?
+
+}
+
---
+++ hosts/loki/hyprland.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "pidof hyprlock || hyprlock";
+        before_sleep_cmd = "loginctl lock-session";
+        after_sleep_cmd = "hyprctl dispatch dpms on";
+        ignore_dbus_inhibit = false;
+      };
+
+      listener = [
+        {
+          timeout = 30;
+          on-timeout = ''brightnessctl --device="kbd_backlight" set 0'';
+          on-resume = ''brightnessctl --device="kbd_backlight" set 180'';
+        }
+        {
+          timeout = 60;
+          on-timeout = "hyprlock --grace 10";
+        }
+        {
+          timeout = 120;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
+  wayland.windowManager.hyprland.settings = {
+    input = {
+      touchpad = {
+        natural_scroll = true;
+        clickfinger_behavior = true;
+      };
+    };
+    monitor = [
+        "eDP-1,highres,0x0,1.5"
+        "HDMI-A-1,highres,auto-left,1"
+    ];
+    gesture = [
+      "3, horizontal, workspace"
+    ];
+    xwayland = {
+      force_zero_scaling = true;
+    };
+    bindle = [
+      ''ALT, XF86MonBrightnessUp, exec, brightnessctl --device="tpacpi::kbd_backlight" s 10%+''
+      ''ALT, XF86MonBrightnessDown, exec, brightnessctl --device="tpacpi::kbd_backlight" s 10%-''
+    ];
+    exec-once = [
+      "steam -silent"
+      "nm-applet"
+    ];
+    debug = {
+      disable_scale_checks = true;
+    };
+  };
+}
