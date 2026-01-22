67f0955e6e5425af6193100a3da83fe1d8e2f0c6
Ulysse Corne <ulysse@corne.sh>
2026-01-22 18:12:33 -0500

    [vscode] config update : disable copilot, add jupyter and sftp extensions

--- vscode.nix
+++ vscode.nix
@@ -16,12 +16,20 @@
         extensions = [
           pkgs.vscode-extensions.enkia.tokyo-night
           pkgs.vscode-extensions.ms-python.python
+          pkgs.vscode-extensions.ms-toolsai.jupyter
           pkgs.vscode-extensions.ms-vscode.cpptools
           pkgs.vscode-extensions.llvm-vs-code-extensions.vscode-clangd
           pkgs.vscode-extensions.jnoortheen.nix-ide
           pkgs.vscode-extensions.mkhl.direnv
           pkgs.vscode-extensions.github.copilot
           pkgs.vscode-extensions.github.copilot-chat
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          {
+            name = "sftp";
+            publisher = "natizyskunk";
+            version = "1.16.3";
+            sha256 = "HifPiHIbgsfTldIeN9HaVKGk/ujaZbjHMiLAza/o6J4=";
+          }
         ];
         userSettings = {
           "window.titleBarStyle" = "custom";
@@ -30,7 +38,10 @@
           "editor.fontFamily" = "Hack Nerd Font";
           "editor.fontSize" = 13;
           "git.confirmSync" = false;
-          "github.copilot.nextEditSuggestions.enabled" = true;
+          "github.copilot.enable" = {
+            "*" = false;
+          };
+          "github.copilot.nextEditSuggestions.enabled" = false;
           "github.copilot.chat.byok.ollamaEndpoint" = "http://10.10.10.12:11434";
           "C_Cpp.intelliSenseEngine" = "disabled";
         };
