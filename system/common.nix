9c4e236c9d2f71f1e9605252864bf1b129e9d13d
Ulysse Corne <ulysse@corne.sh>
2026-02-27 14:22:02 -0500

    [overlays] move overlays to overlays.nix

--- system/common.nix
+++ system/common.nix
@@ -5,6 +5,7 @@
   imports =
     [
       ./dev.nix
+      ./overlays.nix
     ];
 
   # ── Nix Settings ────────────────────────────────────────────────────
@@ -131,26 +132,6 @@
   programs.zsh.enable = true;
   programs.direnv.enable = true;
   
-  # ── Overlays ────────────────────────────────────────────────────────
-  nixpkgs.overlays = [ (
-    final: prev: {
-      # Dolphin fix for MIME apps support
-      # https://discourse.nixos.org/t/dolphin-does-not-have-mime-associations/
-      kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
-          dolphin = kprev.dolphin.overrideAttrs (oldAttrs: {
-            nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
-            postInstall = (oldAttrs.postInstall or "") + ''
-              wrapProgram $out/bin/dolphin \
-                  --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
-                  --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
-            '';
-          });
-        });
-    }
-    ) 
-  ];
-
-
   # ── Dynamic Libraries ───────────────────────────────────────────────
   programs.nix-ld = {
     enable = true; # unpatched dynamic libraries support
