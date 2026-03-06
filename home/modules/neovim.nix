8bf5d43fd64b76eeab05f70f7b0eda4977ba70d6
Ulysse Corne <ulysse@corne.sh>
2026-03-06 16:08:18 -0500

    [lazyvim] add nix and python config

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -4,5 +4,27 @@
   imports = [ lazyvim.homeManagerModules.default ];
   programs.lazyvim = {
     enable = true;
+
+    extras = {
+        lang.nix = {
+          enable = true;
+          installDependencies = true;
+        };
+        lang.python.enable = true;
+    };
+
+    treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
+      nix
+      python
+    ];
+
+    extraPackages = with pkgs; [
+      # LSP servers
+      nixd
+      pyright
+
+      # Linters
+      statix
+    ];
   };
 }
