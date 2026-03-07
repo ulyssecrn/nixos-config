bbba5a4c62de76297d319f3b47b0bc92c14742ef
Ulysse Corne <ulysse@corne.sh>
2026-03-06 20:38:06 -0500

    [lazyvim] add c and tex config + neo_tree and telescope plugins

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -6,22 +6,53 @@
     enable = true;
 
     extras = {
-        lang.nix = {
+      lang = {
+        nix = {
           enable = true;
           installDependencies = true;
         };
-        lang.python.enable = true;
+
+        python = {
+          enable = true;
+          installDependencies = true;
+        };
+
+        clangd = {
+          enable = true;
+          installDependencies = true;
+        };
+
+        tex.enable = true;
+      };
+
+      editor = {
+        neo_tree.enable = true;
+        telescope.enable = true;
+      };
+
+      ai = {
+        claudecode.enable = true;
+        copilot.enable = true;
+        copilot_chat.enable = true;
+      };
     };
 
     treesitterParsers = with pkgs.vimPlugins.nvim-treesitter-parsers; [
       nix
       python
+      c
+      cpp
+      make
+      latex
+      bibtex
     ];
 
     extraPackages = with pkgs; [
       # LSP servers
       nixd
       pyright
+      clang-tools
+      texlab
 
       # Linters
       statix
