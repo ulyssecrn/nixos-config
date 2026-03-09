e19d564b580d3c85c557fdbb3a64abfba3b374d7
Ulysse Corne <ulysse@corne.sh>
2026-03-09 14:42:47 -0400

    [neovim] add transfer plugin

--- home/modules/neovim.nix
+++ home/modules/neovim.nix
@@ -57,6 +57,7 @@
 
       # Tools
       ripgrep
+      rsync
     ];
 
     config = {
@@ -89,6 +90,38 @@
             opts = {
               spec = {
                 { "<leader>v", group = "uv", icon = { icon = "", color = "purple" } },
+              },
+            },
+          },
+        }
+      '';
+      transfer = ''
+        return {
+          {
+            "coffebar/transfer.nvim",
+            lazy = true,
+            cmd = {
+              "TransferInit",
+              "DiffRemote",
+              "TransferUpload",
+              "TransferDownload",
+              "TransferDirDiff",
+              "TransferRepeat"
+            },
+            opts = {},
+            keys = {
+                { "<leader>td", "<cmd>TransferDownload<cr>", desc = "Download from remote" },
+                { "<leader>tf", "<cmd>DiffRemote<cr>", desc = "Diff with remote" },
+                { "<leader>ti", "<cmd>TransferInit<cr>", desc = "Init Deployment config" },
+                { "<leader>tr", "<cmd>TransferRepeat<cr>", desc = "Repeat transfer" },
+                { "<leader>tu", "<cmd>TransferUpload<cr>", desc = "Upload to remote" },
+            },
+          },
+          {
+            "folke/which-key.nvim",
+            opts = {
+              spec = {
+                { "<leader>t", group = "transfer", icon = "" },
               },
             },
           },
