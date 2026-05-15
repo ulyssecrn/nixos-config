0b0cb3c4d338a2d186fc63399fc89222c4a08f22
Ulysse Corne <ulysse@corne.sh>
2026-05-15 10:08:57 -0400

    [hannibal][dnscrypt] fix missing cache file

--- hosts/hannibal/services/dnscrypt-proxy.nix
+++ hosts/hannibal/services/dnscrypt-proxy.nix
@@ -26,6 +26,7 @@
         urls = [
           "https://download.dnscrypt.info/resolvers-list/v3/public-resolvers.md"
         ];
+        cache_file = "/var/cache/dnscrypt-proxy/public-resolvers.md";
         minisign_key = "RWQf6LRCGA9i53mlYecO4IzT51TGPpvWucNSCh1CBM0QTaLn73Y7GFO3";
         refresh_delay = 73; # hours
       };
