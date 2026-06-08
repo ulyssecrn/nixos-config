3f33ef3ced70bbc7458769b20cd3904e14e69b55
Ulysse Corne <ulysse@corne.sh>
2026-06-08 08:59:42 +0200

    [caddy] odysseus reverse proxy

--- hosts/atilla/services/caddy.nix
+++ hosts/atilla/services/caddy.nix
@@ -18,6 +18,7 @@
       "http://backrest.corne.sh".extraConfig     = "reverse_proxy localhost:9898";
       "http://tracearr.corne.sh".extraConfig     = "reverse_proxy localhost:3000";
       "http://openwebui.corne.sh".extraConfig    = "reverse_proxy 10.10.10.9:8050";
+      "http://odysseus.corne.sh".extraConfig     = "reverse_proxy 10.10.10.9:7000";
     };
   };
 
