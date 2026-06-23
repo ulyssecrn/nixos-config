097b1ff516735523fe0d5ccbba9aec44e0c4cf58
Ulysse Corne <ulysse@corne.sh>
2026-06-23 23:12:51 +0200

    [atilla][vpn] fix ip forwarding

--- hosts/atilla/services/protonvpn-fr.nix
+++ hosts/atilla/services/protonvpn-fr.nix
@@ -67,11 +67,9 @@
       # would re-add rules; cleaner to just turn it off in this container.
       networking.firewall.enable = false;
 
-      # enable ip forwarding to prevent tailscale warnings on the web interface
-      # even tough it works without it
-      networking.ipForward = true;
-
       services.tailscale.enable = true;
+      # Enables ip_forward so Tailscale recognizes exit node capability.
+      services.tailscale.useRoutingFeatures = "server";
 
       # UDP GRO offload on the veth — required for non-terrible exit-node
       # throughput per https://tailscale.com/s/ethtool-config-udp-gro.
