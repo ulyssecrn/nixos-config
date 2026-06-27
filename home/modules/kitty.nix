a441cf63370fa2ca4df5fa5e85ca8ba4cf73dccf
Ulysse Corne <ulysse@corne.sh>
2026-06-27 14:06:34 +0200

    [kitty] copy on select

--- home/modules/kitty.nix
+++ home/modules/kitty.nix
@@ -8,6 +8,8 @@
     settings = {
       window_margin_width = "3 5 3";
       confirm_os_window_close = "0";
+      copy_on_select = "yes";   # selecting text copies it to the clipboard
+
     };
   };
 }
