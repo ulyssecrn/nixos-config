c6d3a7f2d4b28b77c4a35067b376ca35b5d9cf96
Ulysse Corne <ulysse@corne.sh>
2026-09-10 11:30:04 -0400

    [nextcloud] bump to 33.0.8

--- hosts/atilla/services/nextcloud.nix
+++ hosts/atilla/services/nextcloud.nix
@@ -32,7 +32,7 @@ let
   '';
 
   # Pin the same image for the web and cron containers so they never skew.
-  nextcloudImage = "docker.io/library/nextcloud:33.0.7-apache";
+  nextcloudImage = "docker.io/library/nextcloud:33.0.8-apache";
 in
 {
   # Nextcloud on the OFFICIAL nextcloud:*-apache image + MariaDB. Was the
